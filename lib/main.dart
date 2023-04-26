6498b038c6537cd41f22ae8d1236a31cd747c392
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-04-26 18:23:47 +0530

    1

---
+++ lib/main.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Color BiscuitGrey = Color.fromRGBO(47, 47, 47, 1.0);
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.orangeAccent,
+          child: ListView(
+              children: [
+                Stack(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height/2.5,
+                          color: Colors.orangeAccent,
+                        ),
+                        Container(
+                          height: MediaQuery.of(context).size.height/1.5,
+                          margin: EdgeInsets.all(0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: BiscuitGrey,
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 50,
+                                margin: EdgeInsets.all(50),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(color: BiscuitGrey, width: 2),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Positioned(
+                      top: MediaQuery.of(context).size.height/3+10,
+                      left: MediaQuery.of(context).size.width/2+30,
+                      right: 0,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            // Navigate to the new screen/widget on button click
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Chat()),
+                            );
+                          },
+                          child: Text('Button'),
+                          style: ElevatedButton.styleFrom(
+                            shape: CircleBorder(),
+                            minimumSize: Size(100,100),
+                            primary: BiscuitGrey
+                          ),
+                      ),
+                    ),
+                  ],
+                ),
+              ]
+          ),
+        ),
+    );
+  }
+}
+
+class Chat extends StatelessWidget {
+  Color BiscuitGrey = Color.fromRGBO(47, 47, 47, 1.0);
+  final List<Map<String, dynamic>> messages = [
+    {'text': 'Hello!', 'isMe': true},
+    {'text': 'Hi there!', 'isMe': false},
+    {'text': 'How are you?', 'isMe': true},
+    {'text': 'I am good, thanks!', 'isMe': false},
+    {'text': 'What are you up to today?', 'isMe': false},
+    {'text': 'Not much, just coding!', 'isMe': true},
+    {'text': 'That sounds fun!', 'isMe': false},
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Chat App'),
+
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                final message = messages[index];
+                return Container(
+                  alignment: message['isMe'] ? Alignment.centerRight : Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                  color: BiscuitGrey,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: message['isMe'] ? Colors.green[300] : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                    child: Text(
+                      message['text'],
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            margin: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Type a message...',
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
