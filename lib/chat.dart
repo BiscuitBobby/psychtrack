a4cbb352364d593eac4ac17a258d1eb9b64fe371
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-05-08 00:09:54 +0530

    Almost lost all my progress, learnt to reverse engineer apk and make sure I keep pushing my projects in the future

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+bool isTyping = false;
+bool isTyping0 = false;
+bool isTyping1 = false;
+bool isTyping2 = false;
 
 class Chat extends StatefulWidget {
   @override
@@ -8,10 +14,12 @@ class Chat extends StatefulWidget {
 }
 
 class _MainPageState extends State<Chat> {
-  Color biscuitGrey = Color.fromRGBO(47, 47, 47, 1.0);
+  final ScrollController _scrollController = ScrollController();
+  Color biscuitGrey = const Color.fromRGBO(47, 47, 47, 1.0);
+  Color BiscuitOrange = const Color.fromRGBO(240, 174, 131, 1.0);
   final List<Map<String, dynamic>> messages = [];
 
-  TextEditingController _messageController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
 
   void _sendMessage(String message) async {
     String url = 'https://react.biscuitbobby.me/chat';
@@ -22,6 +30,34 @@ class _MainPageState extends State<Chat> {
       messages.add({'text': message, 'isMe': true, 'icon':'assets/user.png'});
     });
     _messageController.clear();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 200),
+        curve: Curves.easeOut,
+      );
+    });
+
+    isTyping = true;
+
+    Timer blink = Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      setState(() {
+        isTyping0 = !isTyping0;
+      });
+    });
+    Timer blink1 = Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      Future.delayed(const Duration(milliseconds: 250)).then((value) {
+        setState(() {
+          isTyping1 = !isTyping1;});
+      });
+    });
+    Timer blink2 = Timer.periodic(const Duration(milliseconds: 500), (timer) {
+      Future.delayed(const Duration(milliseconds: 450)).then((value) {
+        setState(() {
+          isTyping2 = !isTyping2;});
+      });
+    });
 
     http.Response response = await http.post(Uri.parse(url), headers: headers, body: json.encode(body));
 
@@ -34,23 +70,46 @@ class _MainPageState extends State<Chat> {
       });
 
       _messageController.clear();
+
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOut,
+        );
+      });
     } else {
       setState(() {
-        messages.add({'text': 'Error: ${response.statusCode}', 'isMe': false});
+        messages.add({'text': 'Unable to reach server \n[Error: ${response.statusCode}]', 'isMe': false, 'icon':'assets/logo.png'});
+      });
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOut,
+        );
       });
     }
+    isTyping = false;
+    blink.cancel();
+    blink1.cancel();
+    blink2.cancel();
+    isTyping0 = false;
+    isTyping1 = false;
+    isTyping2 = false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+      appBar: AppBar(backgroundColor: biscuitGrey,),
       body: Container(
         color: biscuitGrey,
         child: Column(
           children: [
             Expanded(
               child: ListView.builder(
+                controller: _scrollController,
                 itemCount: messages.length,
                 itemBuilder: (context, index) {
                   final message = messages[index];
@@ -72,41 +131,79 @@ class _MainPageState extends State<Chat> {
                             ),
                           Expanded(
                               child: Container(
-                              alignment: message['isMe'] ? Alignment.centerRight : Alignment.centerLeft,
-                              padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
-                              child: Container(
-                                  constraints: BoxConstraints(
-                                    maxWidth: MediaQuery.of(context).size.width-125,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: message['isMe'] ? Colors.green[300] : Colors.grey[300],
-                                    borderRadius: BorderRadius.circular(8.0),
-                                  ),
-                                  padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                                  alignment: message['isMe'] ? Alignment.centerRight : Alignment.centerLeft,
+                                  padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
+                                  child: Container(
+                                      constraints: BoxConstraints(
+                                        maxWidth: MediaQuery.of(context).size.width-125,
+                                      ),
+                                      decoration: BoxDecoration(
+                                        color: message['isMe'] ? BiscuitOrange : Colors.grey[300],
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
 
-                                  child: Text(
-                                    message['text'],
-                                    style: TextStyle(fontSize: 16.0),)))),
-                          if (message['isMe'])
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: CircleAvatar(
-                                radius: 21.0,
-                                backgroundImage: AssetImage(
-                                message['icon'],
-                                ),
-                              ),
-                            ),
+                                      child: Text(
+                                        message['text'],
+                                        style: TextStyle(fontSize: 16.0),)))),
+
                         ]
                     ),
                   );
                 },
               ),
             ),
+            Visibility(
+              visible: isTyping,
+              child: Row(
+                children: [
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 8, vertical: 15)),
+                  Visibility(
+                    visible: isTyping0,
+                    child: Container(
+                      width: 20.0,
+                      height: 20.0,
+                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: RefreshProgressIndicator(
+                        backgroundColor: Colors.grey[300],
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                      ),
+                    ),
+                  ),
+
+                  Visibility(
+                    visible: isTyping1,
+                    child: Container(
+                      width: 20.0,
+                      height: 20.0,
+                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: RefreshProgressIndicator(
+                        backgroundColor: Colors.grey[300],
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                      ),
+                    ),
+                  ),
+
+                  Visibility(
+                    visible: isTyping2,
+                    child: Container(
+                      width: 20.0,
+                      height: 20.0,
+                      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: RefreshProgressIndicator(
+                        backgroundColor: Colors.grey[300],
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
             Container(
               decoration: BoxDecoration(
                 color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(8.0),
+                borderRadius: BorderRadius.circular(20.0),
               ),
               margin: const EdgeInsets.all(8.0),
               child: Row(
@@ -114,22 +211,30 @@ class _MainPageState extends State<Chat> {
                   Expanded(
                     child: TextField(
                       controller: _messageController,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         border: InputBorder.none,
                         hintText: 'Type a message...',
-                        contentPadding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        contentPadding: EdgeInsets.symmetric(horizontal: 12.0),
                       ),
                     ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.send),
-                    onPressed: () {
-                      String message = _messageController.text;
-                      if (message.isNotEmpty) {
-                        _sendMessage(message);
-                      }
-                    },
+                  Container(
+                    decoration: BoxDecoration(
+                      color: BiscuitOrange,
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () {
+                        String message = _messageController.text;
+                        if (message.isNotEmpty) {
+                          _sendMessage(message);
+                        }
+                      },
+                    ),
                   ),
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 2),)
                 ],
               ),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'chat.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Temporary title',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -23,82 +28,271 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   Color BiscuitGrey = Color.fromRGBO(47, 47, 47, 1.0);
-
+  Color BiscuitOrange = const Color.fromRGBO(240, 174, 131, 1.0);
+  String _getGreeting() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Good morning,';
+    } else if (hour < 16) {
+      return 'Good afternoon,';
+    } else {
+      return 'Good evening,';
+    }
+  }
+  bool _isServerOnline = false;
+  Future<void> _checkServerStatus() async {
+    try {
+      final response = await http.get(
+          Uri.parse('https://react.biscuitbobby.me/active'));
+      if (response.statusCode == 200) {
+        setState(() {
+          _isServerOnline = true;
+        });
+      } else {
+        setState(() {
+          _isServerOnline = false;
+        });
+      }
+    } catch (e) {
+      setState(() {
+        print('error getting status');
+        _isServerOnline = false;
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
+    final DateTime now = DateTime.now();
+    _checkServerStatus();
+    final List<DateTime> dates = [
+      now.subtract(Duration(days: 1)),
+      now,
+      now.add(Duration(days: 1)),
+      now.add(Duration(days: 2)),
+    ];
     return Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.orangeAccent,
-          child: ListView(
-              children: [
-                Stack(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          height: MediaQuery.of(context).size.height/2.5,
-                          color: Colors.orangeAccent,
-                        ),
-                        Container(
-                          height: MediaQuery.of(context).size.height/1.5,
-                          margin: EdgeInsets.all(0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: BiscuitGrey,
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 50,
-                                margin: EdgeInsets.all(50),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  border: Border.all(color: Colors.indigoAccent, width: 2),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                      top: MediaQuery.of(context).size.height/3+10,
-                      left: MediaQuery.of(context).size.width/2+30,
-                      right: 0,
-                      child: ElevatedButton(
-                          onPressed: () {
-                            // Navigate to the new screen/widget on button click
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => Chat()),
-                            );
-                          },
-                          child: Text('Button'),
-                          style: ElevatedButton.styleFrom(
-                            shape: CircleBorder(),
-                            minimumSize: Size(100,100),
-                            primary: BiscuitGrey
-                          ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: BiscuitGrey,
+        child: ListView(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                      top: 0,
+                      child: Image.asset(
+                          'assets/character.png',
+                          width: MediaQuery.of(context).size.width)),
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height/2.5,
                       ),
+                      Container(
+                        height: MediaQuery.of(context).size.height/1.5,
+                        margin: EdgeInsets.all(0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: BiscuitGrey,
+                        ),
+                        child: Column(
+                          children: [
+                            const Padding(
+                                padding: EdgeInsets.symmetric(
+                                    vertical: 12.0)),
+                            Row(
+                              children: [
+                                const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 12.0)),
+                                CircleAvatar(
+                                  radius: MediaQuery.of(context).size.width/10,
+                                  backgroundImage: const AssetImage(
+                                    'assets/logo.png',
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                      child: Text(
+                                        _getGreeting(),
+                                        style: const TextStyle(
+                                          fontSize: 18.0,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                      ),
+                                      child: const Text(
+                                        'BiscuitBobby',
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  height: 30,
+                                  width: MediaQuery.of(context).size.width/4,
+                                  margin: const EdgeInsets.all(40),
+                                  padding: const EdgeInsets.symmetric(vertical: 2),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    border: Border.all(color: Colors.white, width: 2),
+                                  ),
+                                  child: Text(
+                                    TimeOfDay.now().format(context),
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 30,
+                                  padding: const EdgeInsets.symmetric(vertical: 2),
+                                  width: MediaQuery.of(context).size.width/2,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(30),
+                                      border: Border.all(color: Colors.white, width: 2),
+                                      color: Colors.white
+                                  ),
+                                  child: Text(
+                                    _isServerOnline ? 'Server status: Active' : 'Server status: Offline',
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18.0,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(30.0),
+                                  child: Container(
+                                    height: 90,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(20)),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: dates.map((date) {
+                                            final bool isToday = date.day == now.day;
+                                            return Container(
+                                              padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 6),
+                                              child: Column(
+                                                children: [
+                                                  Container(
+                                                    width: isToday?MediaQuery.of(context).size.width/7 : MediaQuery.of(context).size.width/13,
+                                                    decoration: BoxDecoration(
+                                                      color: isToday ? BiscuitOrange : null,
+                                                      borderRadius: BorderRadius.circular(12.0),
+                                                    ),
+                                                    height: 75,
+                                                    child: Column(
+                                                      children: [
+                                                        Padding(padding: const EdgeInsets.symmetric(vertical: 6),),
+                                                        Text(
+                                                          DateFormat('EEEE').format(date)[0]+DateFormat('EEEE').format(date)[1]+DateFormat('EEEE').format(date)[2],
+                                                          style: TextStyle(
+                                                            fontSize: 14.0,
+                                                            fontWeight: FontWeight.bold,
+                                                            color: isToday ? Colors.white : null,
+                                                          ),
+                                                        ),
+                                                        Padding(padding: const EdgeInsets.symmetric(vertical: 2),),
+                                                        Text(
+                                                          DateFormat('d').format(date),
+                                                          style: TextStyle(
+                                                            fontSize: 26.0,
+                                                            fontWeight: FontWeight.bold,
+                                                            color: isToday ? Colors.white : null,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            );
+                                          }).toList(),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 100,
+                                  width: MediaQuery.of(context).size.width/3.8,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    border: Border.all(color: Colors.white, width: 2),
+                                  ),
+                                  child: Text('.'
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height/3+15,
+                    left: MediaQuery.of(context).size.width/2+30,
+                    right: 0,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Navigate to the new screen/widget on button click
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Chat()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          minimumSize: const Size(80,80),
+                          backgroundColor: BiscuitGrey
+                      ),
+                      child: Image.asset(
+                          'assets/message.png',
+                          scale: 10),
                     ),
-                  ],
-                ),
-              ]
-          ),
+                  ),
+                ],
+              ),
+            ]
         ),
+      ),
     );
   }
 }
-
